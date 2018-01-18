6736ad141a0c47fccc2c6b2c525d213da3a19591
oscar <oscargdlp@gmail.com>
2018-01-18 18:49:32 +0100

    Fichero markdown con resultados

--- pruebaModelo.py
+++ pruebaModelo.py
@@ -1,16 +1,22 @@
-from sklearn.preprocessing import LabelEncoder
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import RandomizedSearchCV, cross_val_score, StratifiedKFold
-
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import LabelEncoder
 
 if __name__ == '__main__':
 
     lenc = LabelEncoder()
-    data=pd.read_csv('./Datasets/SP1-2016_data.csv')
+    data1=pd.read_csv('./Datasets/SP1-2012_normAverage.csv')
+    data2 = pd.read_csv('./Datasets/SP1-2013_normAverage.csv')
+    data3 = pd.read_csv('./Datasets/SP1-2014_normAverage.csv')
+    data4 = pd.read_csv('./Datasets/SP1-2015_normAverage.csv')
+    data5 = pd.read_csv('./Datasets/SP1-2016_normAverage.csv')
 
-    y_df = data['HTR']
-    x_df = data.loc[:,data.columns != 'HTR']
+    data =  pd.concat([data1,data2, data3, data4, data5])
+
+    y_df = data['FTR']
+    x_df = data.loc[:,data.columns != 'FTR']
     y = lenc.fit_transform(np.array(y_df))
     X = np.array(x_df.loc[:,[True if c not in  ['Div','Date','HomeTeam','AwayTeam','HTTeam','ATTeam'] else False for c in x_df.columns]])
 
@@ -26,4 +32,10 @@ if __name__ == '__main__':
 
 
     # Cross Validation a usar y numero maximo de iteraciones
-    cv = StratifiedKFold(n_splits=10, shuffle=True).split(X, y)
+
+
+    clf = RandomForestClassifier(bootstrap=True, class_weight= None, criterion= 'entropy', max_depth= 6, min_samples_leaf= 2, min_samples_split= 10, n_estimators= 1000)
+    scores = cross_val_score(clf, X, y)
+
+    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
